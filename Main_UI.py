64e238321a1c747b5967d7b6bd1648aaabb62f69
chijunyou <junyouchi@gmail.com>
2019-09-28 22:09:20 -0400

    Add UI file and some small changes

---
+++ Main_UI.py
@@ -0,0 +1,70 @@
+import sys
+import GetTwitterFunction
+import google_sample1
+from PyQt5.QtWidgets import QPushButton, QApplication, QWidget, QLineEdit, QLabel, QLCDNumber
+
+
+class WinForm(QWidget):
+    def __init__(self, parent=None):
+        super(WinForm, self).__init__(parent)
+        self.setGeometry(300, 300, 350, 350)
+        self.setWindowTitle('APP Name')
+        self.Run = QPushButton('Run', self)
+        self.Run.setGeometry(250, 100, 80, 40)
+        self.Run.setStyleSheet("background-color: green")
+        self.Run.clicked.connect(self.run)
+
+        self.Inf = QLabel(self)
+        self.Inf.move(40, 40)
+        self.Inf.resize(200, 40)
+        self.Inf.setText('Please type in something')
+
+
+        self.Inline = QLineEdit(self)
+        self.Inline.move(40,100)
+        self.Inline.resize(200,40)
+
+        self.Outline = QLCDNumber(self)
+        self.Outline.move(90,170)
+        self.Outline.resize(150,80)
+        self.Outline.setDigitCount(2)
+        self.Outline.display(00)
+
+        self.Run.setDisabled(True)
+        self.Inline.textChanged.connect(self.disableRun)
+
+    def disableRun(self):
+
+        self.Run.setDisabled(True)
+
+        for ele in self.Inline.text():
+            if ele != ' ':
+                self.Run.setDisabled(False)
+
+
+
+    def run(self):
+        if self.Inline.isEmpty() != 1:
+
+            self.Inf.setText('Pleas waiting........')
+            analyze(self.Inline.Text())
+            self.Outline.display(99)
+            self.Inf.setText('Please type in something')
+
+
+def analyze(txtin):
+    eles = getTwitterFeed(txtin)
+    sum = 0
+    for ele in eles :
+        sum = sum + sample_analyze_sentiment(ele)
+    rtn = int(sum / len(eles) * 10 + 50)
+    return rtn
+
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = WinForm()
+    win.show()
+    sys.exit(app.exec_())
